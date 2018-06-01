983fab2c0b74a54697c3943f6fe2f9ad4b9fb713
natrayanp <31285863+natrayanp@users.noreply.github.com>
2018-06-01 21:45:33 +0800

    Update mforderapi.py

--- pf/mforderapi.py
+++ pf/mforderapi.py
@@ -1,15 +1,15 @@
-from pf import app
-from pf import dbfunc as db
-from pf import jwtdecodenoverify as jwtnoverify
+from app import app
+import dbfunc as db
+import jwtdecodenoverify as jwtnoverify
 
 from flask import request, make_response, jsonify, Response, redirect
 from datetime import datetime
-from order import dbfunc as db
-from order import jwtdecodenoverify as jwtnoverify
+import dbfunc as db
+import jwtdecodenoverify as jwtnoverify
 from dateutil import tz
 from datetime import datetime
 from datetime import date
-from pf import settings
+import settings
 from multiprocessing import Process
 from multiprocessing import Pool
 
