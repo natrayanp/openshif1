ebbd1708e1c6044b328fa601f71ae1b822f37e69
natrayanp <31285863+natrayanp@users.noreply.github.com>
2018-06-01 21:12:02 +0800

    Update app.py

--- pf/app.py
+++ pf/app.py
@@ -5,11 +5,11 @@ from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 CORS(app)
 
-from pf import portfolio
-from pf import mforder
-from pf import mfsiporder
-from pf import fund
-from pf import jwtdecodenoverify as jwtnoverify
+import portfolio
+import mforder
+import mfsiporder
+import fund
+import jwtdecodenoverify as jwtnoverify
 
 
 
